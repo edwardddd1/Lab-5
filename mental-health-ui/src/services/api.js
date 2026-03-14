7841bf457f57a9572ca82fafb2c86d15fd4f66c3
Edwardddd1 <edwardjohndomingo1@gmail.com>
2026-03-14 11:51:40 +0800

    fix

--- mental-health-ui/src/services/api.js
+++ mental-health-ui/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lab-5-65sp.onrender.com",
+  baseURL: "https://lab-5-1-9no2.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
