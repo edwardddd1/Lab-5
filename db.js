b16a354b4e69026ea5a7bde1f0209ee6af624ff1
Edwardddd1 <edwardjohndomingo1@gmail.com>
2026-03-14 11:26:01 +0800

    fix

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, // Check if your MySQL has a password
   database: process.env.DB_NAME,
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
